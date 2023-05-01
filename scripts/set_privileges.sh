223dc53ce859a81d1ecdfc9c4612950343b8e374
A.P.Rajshekhar <ap.rajshekhar@gmail.com>
2023-05-01 16:22:36 +0530

    Update set_privileges.sh

--- scripts/set_privileges.sh
+++ scripts/set_privileges.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-chmod +x /home/ec2-user/server/user-services/* jar
-chmod +x /home/ec2-user/server/user-services/server_start.sh
-chmod +x /home/ec2-user/server/user-services/server_stop.sh
+chmod +x /home/ec2-user/server/user-services/*.jar
+chmod +x /home/ec2-user/server/user-services/scripts/server_start.sh
+chmod +x /home/ec2-user/server/user-services/scripts/server_stop.sh
