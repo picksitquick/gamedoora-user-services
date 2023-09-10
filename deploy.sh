c4753926e54b35c3eaf210f043e6728a24f39410
Picksitquick <66877821+picksitquick@users.noreply.github.com>
2023-09-10 17:15:11 +0530

    Changes in CI/CD Process (#41)
    * added DTO mapper for Users Model
    
    * added auto generated DTOs, and APIs are now returning DTOs
    
    * adding mapper for different models and making sure API return only DTOs
    
    * tried refactoring user-service and model repos
    
    * added new DTOs for the db models
    
    * added DAO to DTO Mappers but having problem in package initialization, hence in compilation.
    
    * Moved mapper and DTOs to gamedoora-model
    
    * refactoring packages
    
    * minor refactoring
    
    * added configs for running the application
    
    * changed private fields to setter based autowiring
    
    * refactored naming convention in UserController and added test classes
    
    * updated UserRepositoryTest and its configs
    
    * updated test repository imports
    
    * updates and resolving conflicts
    
    * Updated test repository
    
    * updated deployment files
    
    * removing identity file command
    
    * Changed the status code to 0, and updated env variable name
    
    * Changed the status code to 0, and updated env variable name
    
    * updating workflows, introducing separate workflows for build and build & publish
    
    * Revert "updating workflows, introducing separate workflows for build and build & publish"
    
    This reverts commit 2966f38f4b846e1845d554162d13e1a6a98f9ffd.

--- deploy.sh
+++ deploy.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
 
 JAR_FILE=target/*.jar
-REMOTE_DIRECTORY=/home/ec2-user/server/user-services
-REMOTE_JAR_FILE=user-services.jar
+REMOTE_DIRECTORY=/opt/gamedoora/
+REMOTE_JAR_FILE=gamedoora-user-services.jar
 
-# Create a temporary SSH key file
-echo "$SSH_KEY" > /tmp/gamedoora-dev.pem
-chmod 600 /tmp/gamedoora-dev.pem
-cat /tmp/gamedoora-dev.pem
 echo $SSH_USERNAME
 echo $SSH_HOST
 
-# connect to the EC2 instance and set up the remote directory
-ssh -i "/tmp/gamedoora-dev.pem" -o StrictHostKeyChecking=no "$SSH_USERNAME@$SSH_HOST" "
+# connect to the Server instance and set up the remote directory
+ssh -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_PASSWORD@$SSH_HOST" "
   if [ -d $REMOTE_DIRECTORY ]; then
     cd $REMOTE_DIRECTORY
     if [ -f $REMOTE_JAR_FILE ]; then
-      pkill -f $REMOTE_JAR_FILE
       rm -f $REMOTE_JAR_FILE
     fi
   else
     mkdir -p $REMOTE_DIRECTORY
   fi"
 
+#COPY SYSTEMD INTO LOCAL
+DEST_PATH="/usr/lib/systemd/system/"
+scp gamedoora-user-service.service -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_PASSWORD@$SSH_HOST:$DEST_PATH"
+
+# Stop the service
+ssh -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_PASSWORD@$SSH_HOST" 'sudo systemctl daemon-reload; sudo systemctl stop gamedoora-user-services'
+
 # copy the new JAR file to the remote directory and rename it
-scp -i "/tmp/gamedoora-dev.pem" -o StrictHostKeyChecking=no $JAR_FILE "$SSH_USERNAME@$SSH_HOST:$REMOTE_DIRECTORY/$REMOTE_JAR_FILE"
-
-# make the new JAR file executable and start the new service
-ssh -i "/tmp/gamedoora-dev.pem" -o StrictHostKeyChecking=no "$SSH_USERNAME@$SSH_HOST" "
-  cd $REMOTE_DIRECTORY
-  chmod +x $REMOTE_JAR_FILE
-  sudo java -jar -Dserver.port=8081 $REMOTE_JAR_FILE > user_service.log 2> user_service.error.log &"
-
-# Remove the temporary SSH key file
-rm /tmp/gamedoora-dev.pem
+scp $JAR_FILE -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_PASSWORD@$SSH_HOST:$REMOTE_DIRECTORY/$REMOTE_JAR_FILE"
+ssh $SSH_USERNAME:$SSH_PASSWORD@host 'sudo chown gamedoora:gamedoora /opt/gamedoora/\*.jar; sudo systemctl restart gamedoora-user-services'
 
 # Check the exit status of the previous command and set an output variable accordingly
 if [ $? -eq 0 ]; then
